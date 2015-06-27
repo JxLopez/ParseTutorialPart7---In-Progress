c89616b36ac75d58ba5a335292599e5207b409e2
Bizzi <bizzi@bizzi-body.com>
2015-06-27 20:26:00 +0100

    Initial commit

---
+++ ParseTutorial/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  collectionViewCell.swift
+//  ParseTutorial
+//
+//  Created by Ian Bradbury on 23/06/2015.
+//  Copyright (c) 2015 bizzi-body. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+	
+	@IBOutlet weak var cellImage: UIImageView!
+	@IBOutlet weak var cellTitle: UILabel!
+	
+}
